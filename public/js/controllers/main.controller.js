e9472f3bcd84125a43c4e48d6929b8ebaf632d14
Aaron Schwartz <aschwartz628@gmail.com>
2016-11-17 15:55:52 -0500

    added search function and boolean values

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -30,6 +30,10 @@ function MainController($scope, AlbumService){
     AlbumService.update(index, album);
     album.isBeingEdited = false;
   }
+  $scope.changeVote = function(vote, flag) {
+    $scope.vote = vote == flag ? 'None' : flag;
+    alert($scope.vote);
+  };
 
 }
 
